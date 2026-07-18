2b25635545eb673bd73da6cf5350465a87f99f17
Manolito <manuelalen@protonmail.com>
2026-07-18 12:15:13 +0200

    feat:additional changes to dbt run workflow v1.0.22

--- dbt/models/bronze/stg_movimientos.sql
+++ dbt/models/bronze/stg_movimientos.sql
@@ -4,23 +4,21 @@
     unique_key='id'
 ) }}
 
-WITH raw_file AS (
+
+WITH raw_data AS (
     SELECT 
         (regexp_split_to_array(line, ','))[1] as id,
         (regexp_split_to_array(line, ','))[2]::numeric as monto,
         (regexp_split_to_array(line, ','))[3] as concepto,
         (regexp_split_to_array(line, ','))[4]::timestamp as parsed_at
     FROM (
-        SELECT unnest(string_to_array(convert_from(storage.download('raw-data-lake', '{{ var("target_date") | replace("-", "/") }}/movimientos.csv'), 'UTF8'), E'\n')) as line
+        -- Leemos el archivo mediante el path directo al sistema de archivos de Supabase
+        SELECT unnest(string_to_array(convert_from(pg_read_binary_file('/var/lib/postgresql/data/storage/raw-data-lake/' || '{{ var("target_date") | replace("-", "/") }}' || '/movimientos.csv'), 'UTF8'), E'\n')) as line
     ) as t
-    WHERE line NOT LIKE 'id,%' -- Ignora header
+    WHERE line NOT LIKE 'id,%' 
 )
 
-SELECT 
-    id, monto, concepto, parsed_at,
-    '{{ var("target_date") }}' as ingestion_day
-FROM raw_file
-WHERE id IS NOT NULL
+SELECT * FROM raw_data WHERE id IS NOT NULL
 
 {% if is_incremental() %}
   AND parsed_at > (SELECT MAX(parsed_at) FROM {{ this }})
