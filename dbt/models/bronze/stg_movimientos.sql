9c72111dd75c0507bc1442aaf70154cc9e8d4a93
Manolito <manuelalen@protonmail.com>
2026-07-18 12:09:48 +0200

    feat:additional changes to dbt run workflow v1.0.20

--- dbt/models/bronze/stg_movimientos.sql
+++ dbt/models/bronze/stg_movimientos.sql
@@ -6,21 +6,22 @@
 
 WITH raw_file AS (
     SELECT 
-        regexp_split_to_array(line, ',') as columns
+        (regexp_split_to_array(line, ','))[1] as id,
+        (regexp_split_to_array(line, ','))[2]::numeric as monto,
+        (regexp_split_to_array(line, ','))[3] as concepto,
+        (regexp_split_to_array(line, ','))[4]::timestamp as parsed_at
     FROM (
         SELECT unnest(string_to_array(convert_from(storage.download('raw-data-lake', '{{ var("target_date") | replace("-", "/") }}/movimientos.csv'), 'UTF8'), E'\n')) as line
     ) as t
-    WHERE line NOT LIKE 'id,%' -- Saltamos el header
+    WHERE line NOT LIKE 'id,%' -- Ignora header
 )
 
 SELECT 
-    columns[1]::text as id,
-    columns[2]::numeric as monto,
-    columns[3]::text as concepto,
-    columns[4]::timestamp as parsed_at,
+    id, monto, concepto, parsed_at,
     '{{ var("target_date") }}' as ingestion_day
 FROM raw_file
+WHERE id IS NOT NULL
 
 {% if is_incremental() %}
-  WHERE parsed_at > (SELECT MAX(parsed_at) FROM {{ this }})
+  AND parsed_at > (SELECT MAX(parsed_at) FROM {{ this }})
 {% endif %}
